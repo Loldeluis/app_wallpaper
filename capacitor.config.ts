86958e3021b810e698c0ee9cb77c7ad7eed40887
Loldeluis <luismaldonadourib@gmail.com>
2025-09-17 12:54:02 -0500

    primer commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Wallpaper',
+  webDir: 'www'
+};
+
+export default config;
