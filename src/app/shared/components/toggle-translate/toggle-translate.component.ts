c2dc9ed2935b484ac92512e4ef94afe91ffc9fc0
Loldeluis <luismaldonadourib@gmail.com>
2025-09-18 12:00:42 -0500

    toggle no terminado pero medio funcional

--- src/app/shared/components/toggle-translate/toggle-translate.component.ts
+++ src/app/shared/components/toggle-translate/toggle-translate.component.ts
@@ -1,24 +1,38 @@
-import { Component, OnInit, EventEmitter, Output  } from '@angular/core';
+import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   standalone: false,
   selector: 'app-toggle-translate',
   templateUrl: './toggle-translate.component.html',
-  styleUrls: ['./toggle-translate.component.scss'],
+  styleUrls: ['./toggle-translate.component.scss']
 })
-export class ToggleTranslateComponent  implements OnInit {
+export class ToggleTranslateComponent {
+  currentLang: string = 'es';
 
-  constructor() { }
-
-  ngOnInit() {}
-
-    isSpanish: boolean = true;
-
-  @Output() languageChange = new EventEmitter<string>();
-
-  toggleLanguage() {
-    this.isSpanish = !this.isSpanish;
-    this.languageChange.emit(this.isSpanish ? 'es' : 'en');
+  constructor(
+    private translate: TranslateService,
+    private toastCtrl: ToastController
+  ) {
+    this.translate.setDefaultLang(this.currentLang);
   }
-  
+
+  get isSpanish(): boolean {
+    return this.currentLang === 'es';
+  }
+
+  async toggleLang() {
+    this.currentLang = this.currentLang === 'es' ? 'en' : 'es';
+    this.translate.use(this.currentLang);
+
+    const msg = this.translate.instant('LOGIN.LANG_CHANGED');
+    const toast = await this.toastCtrl.create({
+      message: msg,
+      duration: 2000,
+      position: 'top',
+      color: 'primary'
+    });
+    await toast.present();
+  }
 }
--- src/app/shared/components/toggle-translate/toggle-translate.module.ts
+++ src/app/shared/components/toggle-translate/toggle-translate.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToggleTranslateComponent } from './toggle-translate.component';
-
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   declarations: [ToggleTranslateComponent],
-  imports: [CommonModule ],
+  imports: [
+    CommonModule,
+    IonicModule 
+          ],
   exports: [ToggleTranslateComponent]
 })
 export class ToggleTranslateModule { }
