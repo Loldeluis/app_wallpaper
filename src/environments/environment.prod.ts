20846ac73617feb2ff9a6766a9e19f80b6dd4be3
Loldeluis <luismaldonadourib@gmail.com>
2025-09-20 14:30:48 -0500

    build, capacitor, cap hecho

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const environment = {
+    supabase: {
+    supabaseUrl: 'https://vmblneiddobhucxvkovd.supabase.co',
+    supabaseKey:  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZtYmxuZWlkZG9iaHVjeHZrb3ZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc5NTMwNjAsImV4cCI6MjA3MzUyOTA2MH0.bG5Iyq2rN6TwQ-wfRaPJ5S2tVAOdUukBRolxJ3n_h18'
+  },
   production: true,
   firebaseConfig: {
   apiKey: "AIzaSyD1AcEv851w1YSlSX71uDi-890ZfqQXbwo",
